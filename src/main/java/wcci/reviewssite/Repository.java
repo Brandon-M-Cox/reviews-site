04104342607c0acba05bf83c01c2c3590b4eb99e
Isaacjk614 <isaacjk614@gmail.com>
2019-06-12 10:57:34 -0400

    initial controller

--- src/main/java/wcci/reviewssite/Repository.java
+++ src/main/java/wcci/reviewssite/Repository.java
@@ -7,15 +7,29 @@ public class Repository {
 
 	private Map<Long, Review> reviewMap = new HashMap<>();
 
-	public int getReviews() {
-		return reviewMap.size();
-	}
-
-
-//	public void addReview(Review review) {
-//		reviewMap.put(1L)
-//		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//
+//	public int getReviews() {
+//		return reviewMap.size();
 //	}
-
+//
+//	public void addReviews(long id, String title, String imageUrl, String reviewCategory, String reviewContent) {
+//		reviewMap.put(id, new Review(id ,title, imageUrl, reviewCategory, reviewContent));
+//	}
+//
+//
+//
 
 }
--- src/main/java/wcci/reviewssite/ReviewsController.java
+++ src/main/java/wcci/reviewssite/ReviewsController.java
@@ -1,5 +1,34 @@
 package wcci.reviewssite;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ReviewsController {
 
+	Map<Long, Review> reviews = new HashMap<>();
+	
+	@RequestMapping("/reviews/")
+	public String getReviews(Model model) {
+		populateReviews();
+		model.addAttribute("reviews", reviews.values());
+		return "reviews";
+	}
+	@RequestMapping("/reviews/{id}")
+	public String getReview(@PathVariable("id") long id,Model model) {
+		populateReviews();
+		model.addAttribute("review", reviews.get(id));
+		return "review";
+	}
+	private void populateReviews() {
+		if (reviews.isEmpty()) {
+			reviews.put(1L, new Review(1L, "Daniel's Check-ins", "daniel.com", "Candy", "He is the goat of all check-ins"));
+	}
+	
+}
 }
