29b8930262d6fb4bef9bdf57c246f96db745053f
tyler-conner <tyler.conner@gmail.com>
2019-06-14 09:59:06 -0400

    Fixed repository, controller

--- src/main/java/wcci/reviewssite/ReviewRepository.java
+++ src/main/java/wcci/reviewssite/ReviewRepository.java
@@ -11,44 +11,19 @@ public class ReviewRepository {
 
 	Map<Long, Review> reviewMap = new HashMap<>();
 
-//	private void populateReviews() {
-//		if (reviewMap.isEmpty()) {
-//			reviewMap.put(1L,
-//					new Review(1L, "Daniel's Check-ins", "daniel.com", "Candy", "He is the goat of all check-ins"));
-//		}
-//	}
 	public ReviewRepository() {
-		reviewMap.put(1L,
-				new Review(1L, "title", "url", "cat","con"));
-		reviewMap.put(2L, new Review(2L, "title2", "url2", "cat2","con2"));
+		reviewMap.put(1L, new Review(1L, "title", "url", "cat", "con"));
+		reviewMap.put(2L, new Review(2L, "title2", "url2", "cat2", "con2"));
 
 	}
+
 	public Review findReview(Long id) {
 		return reviewMap.get(id);
-		
+
 	}
-	
+
+	public Collection<Review> findAllReviews() {
+		return reviewMap.values();
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-//
-//	public int getReviews() {
-//		return reviewMap.size();
-//	}
-//
-//	public void addReviews(long id, String title, String imageUrl, String reviewCategory, String reviewContent) {
-//		reviewMap.put(id, new Review(id ,title, imageUrl, reviewCategory, reviewContent));
-//	}
-//
-//
-//
 
 }
