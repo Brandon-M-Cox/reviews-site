e503712b385d52d953f60a89784f489ee3e500e4
Isaacjk614 <isaacjk614@gmail.com>
2019-06-20 13:10:31 -0400

    fixed controller test

--- src/test/java/wcci/reviewssite/ReviewsControllerTest.java
+++ src/test/java/wcci/reviewssite/ReviewsControllerTest.java
@@ -4,11 +4,12 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import org.assertj.core.util.Arrays;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -16,9 +17,9 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
-import wcci.library.Book;
-import wcci.library.BookController;
-import wcci.library.BookRepository;
+import wcci.reviewssite.Review;
+import wcci.reviewssite.ReviewsController;
+import wcci.reviewssite.ReviewRepository;
 
 public class ReviewsControllerTest {
 	@InjectMocks
@@ -33,23 +34,25 @@ public class ReviewsControllerTest {
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
 	}
+	Category hodor = new Category();
 
 	@Test
 	public void shouldReturnOneReview() {
-		Review testReview = new Review(1,"Title1","imageUrl1", "content1", "category1");
+		Review testReview = new Review(1,"Title1","imageUrl1", hodor, "content");
 		List<Review > testReviews= Collections.singletonList(testReview);
-		Mockito.when(mockRepo.findAllReviews()).thenReturn(testReviews);
+		Mockito.when(mockRepo.findAll()).thenReturn(testReviews);
 		underTest.getReviews(mockModel);
 		Mockito.verify(mockModel).addAttribute("reviews", testReviews);
 		
 	}
+
 	@Test
 	public void shouldReturnAllReviews() {
-		Review testReview1 = new Review(1,"Title1","imageUrl1", "content1", "category1");
-		Review testReview2 = new Review(2,"Title1","imageUrl1", "content1", "category1");
-		Review testReview3 = new Review(3,"Title1","imageUrl1", "content1", "category1");
-		List<Review > testReviews= Collections.singletonMap(testReview1,testReview2,testReview3);
-		Mockito.when(mockRepo.findAllReviews()).thenReturn(testReviews);
+		Review testReview1 = new Review(1,"Title1","imageUrl1", hodor, "content1");
+		Review testReview2 = new Review(2,"Title1","imageUrl1", hodor, "content2");
+		Review testReview3 = new Review(3,"Title1","imageUrl1", hodor, "content3");
+		List<Review > testReviews= Arrays.asList(testReview1,testReview2,testReview3);
+		Mockito.when(mockRepo.findAll()).thenReturn(testReviews);
 		underTest.getReviews(mockModel);
 		Mockito.verify(mockModel).addAttribute("reviews", testReviews);
 		
