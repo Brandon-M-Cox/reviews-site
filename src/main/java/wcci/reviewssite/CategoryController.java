e35afe8bf380ceee0e93d9cad4f5810b9fecf9f2
nelhag <lamais2006@yahoo.com>
2019-06-24 10:16:42 -0400

    deleted the post package line

--- src/main/java/wcci/reviewssite/CategoryController.java
+++ src/main/java/wcci/reviewssite/CategoryController.java
@@ -4,7 +4,7 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
